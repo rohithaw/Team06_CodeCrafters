c0e99bd5a2414aec0b40f7c39400e2fbe69ae490
VidhyaM09 <vidhyamanickaraj@gmail.com>
2024-07-10 17:12:05 -0500

    Updated excel create,read,write

--- Team06_CodeCrafters/src/test/java/com/utilities/ExcelRead.java
+++ Team06_CodeCrafters/src/test/java/com/utilities/ExcelRead.java
@@ -1,25 +1,54 @@
 package com.utilities;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExcelRead {
 
-	public static void main(String[] args) throws IOException {
-		FileInputStream file = new FileInputStream(
-				"");
-		XSSFWorkbook workbook = new XSSFWorkbook(file);
-		XSSFSheet sheet = workbook.getSheet("Sheet1");
-		int totalrows = sheet.getLastRowNum();
-		for (int i = 1; i < totalrows; i++) {
-			int totalcells = sheet.getRow(i).getLastCellNum();
-			for (int j = 0; j < totalcells; j++) {
-				System.out.println(sheet.getRow(i).getCell(j));
-			}
-		}
-		workbook.close();
-		file.close();
-	}
+    public static List<String> getDataFromExcel(String sheetName, String columnName, String filePath) throws IOException {
+        List<String> columnValues = new ArrayList<>();
+
+        try (FileInputStream fis = new FileInputStream(filePath);
+             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+            XSSFSheet sheet = workbook.getSheet(sheetName);
+
+            int columnIndex = -1;
+            XSSFRow headerRow = sheet.getRow(0);
+            DataFormatter formatter = new DataFormatter();
+
+            // Find the column index
+            for (int i = 0; i < headerRow.getLastCellNum(); i++) {
+                XSSFCell cell = headerRow.getCell(i, XSSFRow.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+                if (cell != null && formatter.formatCellValue(cell).equalsIgnoreCase(columnName)) {
+                    columnIndex = i;
+                    break;
+                }
+            }
+
+            if (columnIndex == -1) {
+                throw new IllegalArgumentException("Column with name " + columnName + " not found");
+            }
+
+            // Iterate through each row in the column
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                XSSFRow row = sheet.getRow(i);
+                if (row != null) {
+                    XSSFCell cell = row.getCell(columnIndex, XSSFRow.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+                    if (cell != null) {
+                        columnValues.add(formatter.formatCellValue(cell));
+                    }
+                }
+            }
+        }
+
+        return columnValues;
+    }
 }
--- Team06_CodeCrafters/src/test/java/com/utilities/ExcelWrite.java
+++ Team06_CodeCrafters/src/test/java/com/utilities/ExcelWrite.java
@@ -1,42 +1,54 @@
 package com.utilities;
 
 import java.io.IOException;
-import java.io.File;
-import java.io.FileNotFoundException;
+
+import java.io.FileInputStream;
+
 import java.io.FileOutputStream;
-import org.apache.poi.ss.usermodel.Cell;
+
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelWrite {
-	public static void writeExcel() throws IOException {
-		// Creating new Workbook
-		XSSFWorkbook Workbook = new XSSFWorkbook();
-		// Creating new Sheet inside the Workbook
-		XSSFSheet Worksheet = Workbook.createSheet("TestData");
-		// row creation
-		int rownum = 0;
-		for (int i = 1; i < 10; i++) {
-			Row row = Worksheet.createRow(rownum++);
-			// column creation
-			int colnum = 0;
-			for (int j = 1; j < 10; j++) {
-				Cell col = row.createCell(colnum++);
-				col.setCellValue("Row" + i + "Column" + j);
-			}
-			// creating excel path
-			String path = System.getProperty("user.dir") + "/src/test/resources/TestData/demo.xlsx";
-			// creating object of file
-			File Excelfile = new File(path);
-			// Writing Excel file using fileoutputstream
-			try {
-				FileOutputStream fos = new FileOutputStream(Excelfile);// Creating object of fileoutputstream
-				Workbook.write(fos);
-				// Workbook.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+	public static void writeToExcel(String sheetName, String recipeId, String recipeName, String recipeCategory,
+			String foodCategory, String ingredients, String prepartionTime, String cookingTime, String tag,
+			String noOfServings, String cuisineCategory, String recipeDescription, String preparationMethod,
+			String nutrientValues, String recipeUrl, String filePath
+
+	) throws IOException {
+
+		FileInputStream fileInputStream = new FileInputStream(filePath);
+		Workbook workbook = WorkbookFactory.create(fileInputStream);
+
+		Sheet sheet = workbook.getSheet(sheetName);
+
+		// Create a new row
+		int lastRowNum = sheet.getLastRowNum();
+		Row row = sheet.createRow(lastRowNum + 1);
+
+		// Write data to the row
+		row.createCell(0).setCellValue(recipeId);
+		row.createCell(1).setCellValue(recipeName);
+		row.createCell(2).setCellValue(recipeCategory);
+		row.createCell(3).setCellValue(foodCategory);
+		row.createCell(4).setCellValue(ingredients);
+		row.createCell(5).setCellValue(prepartionTime);
+		row.createCell(6).setCellValue(cookingTime);
+		row.createCell(7).setCellValue(tag);
+		row.createCell(8).setCellValue(noOfServings);
+		row.createCell(9).setCellValue(cuisineCategory);
+		row.createCell(10).setCellValue(recipeDescription);
+		row.createCell(11).setCellValue(preparationMethod);
+		row.createCell(12).setCellValue(nutrientValues);
+		row.createCell(13).setCellValue(recipeUrl);
+
+		// Write the updated workbook to the file
+		try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+			workbook.write(outputStream);
 		}
+
+		workbook.close();
 	}
 }
