5b27eed69428b029f168a29173505817f6be281c
Limnadavisk <limnadavisk@gmail.com>
2024-07-12 17:12:33 -0400

    Added logic to show full ingredients and recipe description only 100 chars

--- Team06_CodeCrafters/src/test/java/com/tests/recipeData.java
+++ Team06_CodeCrafters/src/test/java/com/tests/recipeData.java
@@ -106,72 +106,70 @@ public class recipeData {
         for (String link : links) {
             getDriver().get(link);
 
-            Document doc = Jsoup.parse(getDriver().getPageSource());
+ Document doc = Jsoup.parse(getDriver().getPageSource());
             
             // Extracting detailed information using Jsoup
             String recipeID = extractNumberFromURL(link);//working
-            //String recipeID = extractRecipeID(doc);
+            
             String recipeName = extractText(doc, "#ctl00_cntrightpanel_lblRecipeName");//working
-            //String recipeCategory = extractText(doc, "#ctl00_cntrightpanel_lblCategory");
-            //String foodCategory = extractText(doc, "#ctl00_cntrightpanel_lblFoodCategory");
-            //String ingredients = extractIngredients(doc);
-            String ingredients = extractText(doc,"[itemprop=recipeIngredient]");
-           // String receipeCategory = driver.findElement(By.xpath("//a[@itemprop='recipeCategory'][1]")).getText();
-            //String recipeCate= extractText(doc,"[itemprop=recipeCategory]");
+            
             String recipeTag = extractText(doc, "[id=recipe_tags]");
             String recipeCategory = extractText(doc, "[itemprop=recipeCategory]");
             String prepTime = extractText(doc, "[itemprop=prepTime]");//working
             String cookTime = extractText(doc, "[itemprop=cookTime]");//working
             //String tag = extractText(doc, "#ctl00_cntrightpanel_lblTags");
             String servings = extractText(doc, "[itemprop=recipeYield]");
-            String cuisineCategory = extractText(doc, "#ctl00_cntrightpanel_lblCuisine");
-            String recipeDescription = extractText(doc, "#recipe-desc");
+            //recipe description logic printing below
+            Element recipeDescription = doc.selectFirst("span#ctl00_cntrightpanel_lblDesc");
+            String recipeDesc = "";
+            if (recipeDescription != null) {
+                recipeDesc = recipeDescription.text();
+                if (recipeDesc.length() > 100) {
+                    recipeDesc = recipeDesc.substring(0, 100);
+                }
+            }
+            //ingredients logic-- printing below
+            Elements ingredients = doc.select("span[itemprop='recipeIngredient'] > a");
+			 StringBuilder ingreList = new StringBuilder();
+	            for (Element ingredient : ingredients) {
+	                ingreList.append(",").append(ingredient.text().toLowerCase());
+	            }
+	            if (ingreList.length() > 0) {
+	                ingreList.deleteCharAt(0);
+	            }
+
             String preparationMethod = extractText(doc, "#ctl00_cntrightpanel_pnlRcpMethod");//working
             String nutrientValues = extractText(doc, "#rcpnutrients"); //working
 
-            // Print or store the extracted information
+            
             System.out.println("Recipe ID: " + recipeID);
             System.out.println("Recipe Name: " + recipeName);
             
-           // String recipeCategory = null;
-			//System.out.println("Recipe Category: " + recipeCategory);
+           
             String categorizedRecipe = categorizeRecipe(recipeCategory, doc);
             System.out.println("Recipe Category: " + categorizedRecipe);
-            //String tag = null;
+           
 			String foodCategory = categorizeFoodType(recipeName, recipeTag);
+			String cuisineCategory = categoryCuisine(recipeName,recipeTag );
 			System.out.println("Food Category: " + foodCategory);
-            System.out.println("Ingredients: " + ingredients);
+          
+			
+	        System.out.println("Ingredients: " + ingreList.toString());
             System.out.println("Preparation Time: " + prepTime);
             System.out.println("Cooking Time: " + cookTime);
             System.out.println("Tag: " + recipeTag);
             System.out.println("No of Servings: " + servings);
             System.out.println("Cuisine Category: " + cuisineCategory);
-            System.out.println("Recipe Description: " + recipeDescription);
+            System.out.println("Recipe Description is: " + recipeDesc);
             System.out.println("Preparation Method: " + preparationMethod);
             System.out.println("Nutrient Values: " + nutrientValues);
             System.out.println("Recipe URL: " + link);//working
 
-            // Go back to the previous page
+            // Go back to the previous page-- loop through recipes
             getDriver().navigate().back();
             raw_recipes = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("rcc_recipename")));
         }
     }
-
-//    private String extractRecipeID(Document doc) {
-//        Elements elements = doc.select(".rcc_rcpno");
-//        System.out.println("Number of elements found for recipe ID: " + elements.size());
-//        for (Element element : elements) {
-//            String fullText = element.text().trim();
-//            System.out.println("Extracted text for recipe ID: " + fullText);
-//            Pattern pattern = Pattern.compile("\\d+");
-//            Matcher matcher = pattern.matcher(fullText);
-//            if (matcher.find()) {
-//                return matcher.group();
-//            }
-//        }
-//        return "";
-//    }
-
     
 
     private String extractText(Document doc, String cssQuery) {
@@ -231,5 +229,84 @@ public class recipeData {
             return "Vegetarian";
         }
     }
-}
+    private static String categoryCuisine(String recipeName, String tag) {
+        if (recipeName.contains("Indian") || tag.contains("Indian")) {
+            return "Indian";
+        } else if (recipeName.contains("South Indian") || tag.contains("South Indian")) {
+            return "South Indian";
+        } else if (recipeName.contains("Rajathani ") || tag.contains("Rajathani ")) {
+            return "Rajathani";
+        } else if (recipeName.contains("Punjabi") || tag.contains("Punjabi")) {
+            return "Punjabi";
+        } else if (recipeName.contains("Bengali") || tag.contains("Bengali")) {
+            return "Bengali";
+        }else if (recipeName.contains("Orissa") || tag.contains("Orissa")) {
+            return "Orissa";
+        }else if (recipeName.contains("Gujarati") || tag.contains("Gujarati")) {
+            return "Gujarati";
+        }else if (recipeName.contains("Maharashtrian") || tag.contains("Maharashtrian")) {
+            return "Maharashtrian";
+        }else if (recipeName.contains("Andhra") || tag.contains("Andhra")) {
+            return "Andhra";
+        }else if (recipeName.contains("Kerala") || tag.contains("Kerala")) {
+            return "Kerala";
+        }else if (recipeName.contains("Goan") || tag.contains("Goan")) {
+            return "Goan";
+        }else if (recipeName.contains("Kashmiri") || tag.contains("Kashmiri")) {
+            return "Kashmiri";
+        }else if (recipeName.contains("Himachali") || tag.contains("Himachali")) {
+            return "Himachali";
+        }else if (recipeName.contains("Tamil nadu") || tag.contains("Tamil nadu")) {
+            return "Tamil nadu";
+        }else if (recipeName.contains("Karnataka") || tag.contains("Karnataka")) {
+            return "Karnataka";
+        }else if (recipeName.contains("Sindhi") || tag.contains("Sindhi")) {
+            return "Sindhi";
+        }else if (recipeName.contains("Chhattisgarhi") || tag.contains("Chhattisgarhi")) {
+            return "Chhattisgarhi";
+        }else if (recipeName.contains("Madhya pradesh") || tag.contains("Madhya pradesh")) {
+            return "Madhya pradesh";
+        }else if (recipeName.contains("Assamese") || tag.contains("Assamese")) {
+            return "Assamese";
+        }else if (recipeName.contains("Manipuri") || tag.contains("Manipuri")) {
+            return "Manipuri";
+        }else if (recipeName.contains("Tripuri") || tag.contains("Tripuri")) {
+            return "Tripuri";
+        }else if (recipeName.contains("Sikkimese") || tag.contains("Sikkimese")) {
+            return "Sikkimese";
+        }else if (recipeName.contains("Mizo") || tag.contains("Mizo")) {
+            return "Mizo";
+        }else if (recipeName.contains("Arunachali") || tag.contains("Arunachali")) {
+        	return "Arunachali";
+        }else if (recipeName.contains("uttarakhand") || tag.contains("uttarakhand"))
+		{
+			return "uttarakhand";
+		}
+		else if (recipeName.contains("Haryanvi") || tag.contains("Haryanvi"))
+		{
+			return "Haryanvi";
+		}
+		else if (recipeName.contains("Awadhi") || tag.contains("Awadhi"))
+		{
+			return "Awadhi";
+		}
+		else if (recipeName.contains("Bihari") || tag.contains("Bihari"))
+		{
+			return "Bihari";
+		}
+		else if (recipeName.contains("Uttar pradesh") || tag.contains("Uttar pradesh"))
+		{
+			return"Uttar pradesh";
+		}
+		else if (recipeName.contains("Delhi") || tag.contains("Delhi"))
+		{
+			return "Delhi";
+		}
+		else
+			return "North Indian";
+	    
+        
+        }
+    }
 
+    
