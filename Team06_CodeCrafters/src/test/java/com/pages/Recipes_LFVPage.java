8b204e90497788ae21553fa811d925b8ccddd1fe
rohithaw <rohithawuppugundur@gmail.com>
2024-07-17 11:59:07 -0700

    Changed Logger File

--- Team06_CodeCrafters/src/test/java/com/pages/Recipes_LFVPage.java
+++ Team06_CodeCrafters/src/test/java/com/pages/Recipes_LFVPage.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
